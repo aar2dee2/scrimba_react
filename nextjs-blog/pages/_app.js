b5c587c8280d4723b6bab9ec63488a22e1d87b17
aar2dee2 <elixirdev.sam@gmail.com>
2022-02-09 21:02:34 +0530

    nextjs sample site

---
+++ nextjs-blog/pages/_app.js
@@ -0,0 +1,8 @@
+import { Component } from "react";
+import styles from "../styles/global.css"
+
+export default function App({ Component, pageProps }) {
+    return (
+        <Component {...pageProps} />
+    )
+}
